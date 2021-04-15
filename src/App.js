fc19d5c37c8441cd6ec03961559ce31eb7bb74da
treuther <tracyreuther@gmail.com>
2021-04-15 18:23:23 -0500

    basic component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text="Welcome to Using Props" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
+  }
 }
 
 export default App;
