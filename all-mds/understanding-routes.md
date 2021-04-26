f2b81288a716dc81e79c86111002ff2599e5e84d
treuther <tracyreuther@gmail.com>
2021-04-26 04:31:20 -0500

    MovieDetail component showing movies

--- all-mds/understanding-routes.md
+++ all-mds/understanding-routes.md
@@ -75,3 +75,20 @@ The prop `exact` is the same as `exact={true}`, but refactored it is just `exact
 
 # Match
 `match` is where params are stored within React-Router
+A match object contains information about how a `<Route path>` matched the URL. `match` objects contain the following properties:
+
+`params` - (object) Key/value pairs parsed from the URL corresponding to the dynamic segments of the path
+`isExact` - (boolean) true if the entire URL was matched (no trailing characters)
+`path` - (string) The path pattern used to match. Useful for building nested `<Route>`s
+`url` - (string) The matched portion of the URL. Useful for building nested `<Link>`s
+
+You’ll have access to match objects in various places:
+
+Route component as `this.props.match`
+Route render as `({ match }) => ()`
+Route children as `({ match }) => ()`
+withRouter as `this.props.match`
+matchPath as the return value
+useRouteMatch as the return value
+
+If a Route does not have a path, and therefore always matches, you’ll get the closest parent match. Same goes for withRouter.
--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MovieDetail extends Component {
 
   state = {
-    movies: []
+    movie: {},
   }
 
   // Study async/await/promises
   async componentDidMount() {
     try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=5111df993a54ce3b4798d6295751075f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate');
-      const movies = await res.json();
-      // console.log(movies.results)
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=5111df993a54ce3b4798d6295751075f&language=en-US`);
+      const movie = await res.json();
+      console.log(movie.id)
+      //`movie: movie` is the same as `movie,` in ES6 update
       this.setState({
-        movies: movies.results
+        movie,
       })
     } catch (e) {
       console.log(e)
@@ -23,9 +26,14 @@ class MovieDetail extends Component {
 
   render() {
     // console.log(this.state.movies)
+    const { movie } = this.state;
     return (
         <div>
-            <h1>Hello</h1>
+            <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt='{movie.title}' />
+            <img src={`${POSTER_PATH}${movie.poster_path}`} alt='{movie.title}' />
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
         </div>
     );
   }
